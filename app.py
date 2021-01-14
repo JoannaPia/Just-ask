95baac879107a203e5b343dc41cc15a816b76cf2
Wojciech Dziadoń <wdziadon@intercars.eu>
2021-01-14 12:39:01 +0100

    list questions and display_question

--- app.py
+++ app.py
@@ -1,172 +1,98 @@
-from datetime import datetime
-
-from flask import Flask, render_template, url_for,  request, redirect
-import note
-import data_manager
+from flask import Flask, render_template, request, url_for, redirect
 import os
+import data_manager
+import datetime
 
-
+HEADERS_PRINT = {"id": "Question ID", "submission_time": "Submission time", "view_number": "View number",
+                 "vote_number": "Vote number", "title": "Title", "message": "Message", "image": "Image"}
+QUESTIONS_HEADERS = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
+ANSWERS_HEADERS = {"id": "Answer ID", "submission_time" : "Submission time", "vote_number": "Vote number",
+                   "question_id": "Question ID", "message": "Message", "image": "Image"}
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 TARGET_FOLDER = 'static/images/'
 UPLOAD_FOLDER = os.path.join(APP_ROOT, TARGET_FOLDER)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = {'png', 'jpg'}
 
 
 @app.route('/')
-@app.route('/list')
 def index():
-    dictionary_keys = note.question_dictionary_keys
-    questions = data_manager.open_file("question.csv")
-    image_names = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template("index.html", headers=dictionary_keys, questions=questions, image_names=image_names)
+    # Fetch all questions with first row being headers ["id", "submission_time", "view_number", "vote_number",
+    # "title", "message", "image"]
+    questions = data_manager.get_all_questions()
+    return render_template("index.html", headers=QUESTIONS_HEADERS, headers_print=HEADERS_PRINT, questions=questions)
 
 
-@app.route('/', methods=["POST"])
-def sort_questions():
-    order_by = request.form['order_by']
-    order_direction = request.form['order_direction']
-    questions = data_manager.sort_questions(order_by, order_direction)
-    dictionary_keys = note.question_dictionary_keys
-    image_names = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template("index.html", headers=dictionary_keys, questions=questions,
-                           order_by=order_by, order_direction=order_direction, image_names=image_names)
-
-
-@app.route('/question/<question_id>')
-def display_question(question_id):
-    dictionary_keys = note.question_dictionary_keys
-    answer_keys = note.answer_keys
-    questions = data_manager.open_file("question.csv")
-    answers = data_manager.open_file("answer.csv") #ZMIENIC
-    display_question = note.get_question(questions, question_id)
-    display_answers = note.get_answer(answers, question_id)
-    image_names = os.listdir(app.config['UPLOAD_FOLDER'])
-    question_image_name = data_manager.return_question_image_name(image_names, question_id)
-    answer_image_names = data_manager.return_answer_image_names(image_names, question_id)
-
-    return render_template("display_question.html", headers=dictionary_keys, questions=display_question,
-                           answers=answer_keys, display_answers=display_answers, question_image_name=question_image_name,
-                           answer_image_names=answer_image_names)
-
-
-@app.route('/edit/<question_id>', methods=["GET"])
-def edit_question(question_id):
-    questions = data_manager.open_file("question.csv")
-    edited_question = note.get_question(questions, question_id)
-    return render_template('edit_question.html', question=edited_question)
-
-
-@app.route('/edit', methods=["POST"])
-def save_edited_question():
-    updated_question = dict(request.form)
-    updated_question_id = updated_question['id']
-    updated_title = updated_question['title']
-    updated_message = updated_question['message']
-    data_manager.save_edited_question(updated_question_id, updated_title, updated_message)
-    return redirect(url_for('display_question', question_id=updated_question_id))
-
-
-@app.route('/add')
+@app.route('/add-question')
 def add_question():
     return render_template('add_question.html')
 
 
-@app.route('/save', methods = ['POST'])
+@app.route('/save', methods=['POST'])
 def save_question():
-
-        title = request.form['title']
-        message = request.form['message']
-        id = data_manager.greatest_id() + 1 #ZMIENIĆ
-        new_question = [str(id),
-                        str(note.data_time_now()), '0', '0', title, message, str(0)]
-        data_manager.add_question('question.csv', ','.join(new_question))
-        return redirect(url_for('display_question', question_id=id))
+    title = request.form['title']
+    message = request.form['message']
+    id = data_manager.add_question(datetime.datetime.now(), '0', '0', title, message, str(0))
+    return redirect(url_for('display_question', question_id=id))
 
 
+@app.route('/<int:question_id>/', methods=['GET'])
+def display_question(question_id):
+    question = data_manager.get_question(question_id)
+    answers = data_manager.get_answers(question_id)
+    return render_template("display_question.html", headers=QUESTIONS_HEADERS, question=question,
+                           answers_headers=ANSWERS_HEADERS, answers=answers, headers_print=HEADERS_PRINT)
 
-@app.route('/question/<question_id>/new-answer')
+
+@app.route('/vote-up/<int:question_id>/<table>')
+def vote_up_on_question(question_id, table):
+    if table == "question":
+        data_manager.vote_up_question(id=question_id)
+    questions = data_manager.get_all_questions()
+    return render_template("index.html", headers=QUESTIONS_HEADERS, headers_print=HEADERS_PRINT, questions=questions)
+
+@app.route('/vote-down/<int:question_id>/<table>')
+def vote_down_on_question(question_id, table):
+    if table == "question":
+        data_manager.vote_down_question(id=question_id)
+    questions = data_manager.get_all_questions()
+    return render_template("index.html", headers=QUESTIONS_HEADERS, headers_print=HEADERS_PRINT, questions=questions)
+
+
+@app.route('/edit_question')
+def edit_question(question_id):
+    pass
+
+
+@app.route('/add_answer')
 def add_answer(question_id):
-    return render_template('add_answer.html', question_id=question_id, answer_keys=note.answer_keys)
+    pass
 
 
-@app.route('/save_answer/<q_id>', methods=['POST'])
-def save_answer(q_id):
-    if request.method == 'POST':
-        message = request.form['message']
-        id_answer = data_manager.greatest_id_answer() + 1
-        #id,submission_time,vote_number,question_id,message,image
-        new_answer = [str(id_answer), str(note.data_time_now()), '0', q_id, message, str(0)]
-
-        data_manager.add_answer('answer.csv', ','.join(new_answer))
-        return redirect(url_for('display_question', question_id=q_id))
-    else:
-        return redirect(url_for('add_answer', question_id=q_id))
-
-
-@app.route('/answer/<answer_id>/delete')
-def delete_answer(answer_id):
-    answer = data_manager.delete_answer(answer_id)
-    return redirect(url_for('display_question', question_id=answer['question_id']))
-
-@app.route('/answer/<answer_id>/vote_up')
-def vote_up_answers(answer_id):
-    q_id = data_manager.vote_up(answer_id)
-    print(q_id)
-    return redirect(url_for('display_question', question_id=q_id))
-
-
-@app.route('/answer/<answer_id>/vote_down')
-def vote_down_answers(answer_id):
-    q_id = data_manager.vote_down(answer_id)
-    return redirect(url_for('display_question', question_id=q_id))
-
-
-@app.route('/vote_question/<question_id>/vote_up')
-def vote_up_on_question(question_id):
-    questions = data_manager.open_file("question.csv")
-    question = note.get_question(questions, question_id)
-    vote_number = int(question["vote_number"]) + 1
-    question["vote_number"] = str(vote_number)
-    data_manager.save_to_file(questions, "question.csv")
-    return redirect(url_for("index"))
-
-
-@app.route('/vote_question/<question_id>/vote_down')
-def vote_down_on_question(question_id):
-    questions = data_manager.open_file("question.csv")
-    question = note.get_question(questions, question_id)
-    vote_number = int(question["vote_number"]) - 1
-    question["vote_number"] = str(vote_number)
-    data_manager.save_to_file(questions, "question.csv")
-    return redirect(url_for("index"))
-
-@app.route('/question/<question_id>/delete')
+@app.route('/delete_question')
 def delete_question(question_id):
-    data_manager.delete_question(question_id)
-    return redirect(url_for('index'))
+    pass
 
 
-@app.route('/question/<question_id>/upload-image', methods=['POST'])
-def upload_image_question(question_id):
-    file = request.files['file']
-    name = file.filename
-    extension = name.split(".")[-1]
-    file_name = "question"+question_id+"."+extension
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
-    return redirect(url_for('display_question', question_id=question_id))
+@app.route('/delete_answer')
+def delete_answer(answer_id):
+    pass
 
 
-@app.route('/question/<question_id>/<answer_id>/upload-image', methods=['POST'])
-def upload_image_answer(question_id, answer_id):
-    file = request.files['file']
-    name = file.filename
-    extension = name.split(".")[-1]
-    file_name = "question"+question_id+"answer"+answer_id+"."+extension
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
-    return redirect(url_for('display_question', question_id=question_id))
+@app.route('/<int:answer_id>/vote-up')
+def vote_up_answers(answer_id):
+    pass
+
+
+@app.route('/<int:answer_id>/vote-down')
+def vote_down_answers(answer_id):
+    pass
+
+
+@app.route('/<int:answer_id>/vote-down')
+def upload_image_answer(answer_id, question_id):
+    pass
 
 
 if __name__ == '__main__':
--- data_manager.py
+++ data_manager.py
@@ -1,179 +1,85 @@
-import csv
-import note
+from typing import List, Dict
 
-answer_file = 'answer.csv'
-from operator import itemgetter
+from psycopg2 import sql
+from psycopg2.extras import RealDictCursor
 
-
-def delete_answer(answer_id):
-    temp_answer = {}
-    answers = open_file('answer.csv')
-    for answer in answers:
-        if answer["id"] == answer_id:
-            temp_answer = answers.pop(answers.index(answer))
-    save_answer('answer.csv', answers)
-    return temp_answer
+import database_common
+headers = {"id": "Question ID", "submission_time": "Submission time", "view_number": "View number"
+            ,"vote_number": "Vote number", "title": "Title", "message": "Message", "image": "Image"}
 
 
-def vote_up(answer_id):
-    answers = open_file(answer_file)
-    for answer in answers:
-        if answer['id'] == answer_id:
-            temp = int(answer['vote_number'])
-            temp+= 1
-            answer['vote_number'] = str(temp)
-            save_answer2('answer.csv', answers)
-            return answer['question_id']
+@database_common.connection_handler
+def get_all_questions(cursor: RealDictCursor) -> dict:
+    query = """
+            SELECT id, submission_time, view_number, vote_number, title
+            FROM question
+            ORDER BY submission_time
+            """
+    cursor.execute(query)
+    questions = cursor.fetchall()
+    return questions
 
-def vote_down(answer_id):
-    answers = open_file(answer_file)
-    for answer in answers:
-        if answer['id'] == answer_id:
-            temp = int(answer['vote_number'])
-            temp -= 1
-            answer['vote_number'] = str(temp)
-            save_answer2('answer.csv', answers)
-            return answer['question_id']
+@database_common.connection_handler
+def add_question(cursor: RealDictCursor, sub, view_n, vote_n, title, mess, image):
+    # update nazwa_tabeli SET nazwa_atrubytu = wartosc WHERE
+    query_max_id = """
+                    SELECT MAX(id) FROM question
+                    """
+    cursor.execute(query_max_id)
+    new_id = cursor.fetchone()
 
-def save_answer2(filename, new_answers):
-    with open(filename, 'w') as f:
-        f.write(','.join(note.answer_keys))
-        for answer in new_answers:
-            #del answer[None]
-            f.write("\n"+','.join(answer.values()))
-        f.close()
+    nid = new_id['max']
 
+    query = "INSERT INTO question " \
+            "VALUES ({},'{}',{},{},'{}','{}','{}')".format(nid+1, sub, view_n, vote_n, title, mess, image)
 
-def delete_question(question_id):
-    questions = open_file('question.csv')
-    for question in questions:
-        if question["id"] == question_id:
-            questions.pop(questions.index(question))
-    save_questions('question.csv', questions)
+    cursor.execute(query)
+    #connection.commit()
+    # conncetion.rollback()
+    # Zeby mozna bylo przekierowac na widok nowo stworzonego pytania
+    return nid+1
 
-
-
-def open_file(filename):
-    with open(filename, "r") as f:
-        reader = csv.DictReader(f)
-        list_of_dict = list(reader)
-
-    return list_of_dict
+@database_common.connection_handler
+def get_question(cursor: RealDictCursor, question_id : str):
+    query = """
+        SELECT *
+        From question
+        WHERE id=%(question_id)s     
+    """
+    param = {'question_id': question_id}
+    cursor.execute(query, param)
+    return cursor.fetchall()
 
 
-def add_question(filename, new_question):
-    with open(filename, 'a') as f:
-        f.write("\n"+new_question)
-        f.close()
+@database_common.connection_handler
+def get_answers(cursor: RealDictCursor, question_id : str):
+    query = """
+        SELECT *
+        From answer
+        WHERE question_id=%(question_id)s
+    """
+    param = {'question_id': question_id}
+    cursor.execute(query, param)
+    return cursor.fetchall()
 
+@database_common.connection_handler
+def vote_up_question(cursor: RealDictCursor, id):
+    query="""
+    UPDATE question
+    SET vote_number = vote_number + 1
+    WHERE id=%(id)s
+    """
+    param = {'id': id}
+    cursor.execute(query, param)
+    return None
 
-def greatest_id():
-    max_id = open_file("question.csv")
-    try:
-        return max([int(index_id['id']) for index_id in max_id])
-    except ValueError as e:
-        return 0
-
-
-def greatest_id_answer():
-    max_id = open_file("answer.csv")
-    try:
-        return max([int(index_id['id']) for index_id in max_id])
-    except ValueError as e:
-        return 0
-
-
-def add_answer(filename, new_answer):
-    with open(filename, 'a') as f:
-        f.write("\n" + new_answer + "\n")
-        f.close()
-
-
-def save_answer(filename, new_answers):
-    with open(filename, 'w') as f:
-        f.write(','.join(note.answer_keys))
-        for answer in new_answers:
-            f.write("\n" + ','.join(answer.values()))
-        f.close()
-
-
-def save_to_file(list_of_dict, filename):
-    keys = list_of_dict[0].keys()
-    with open(filename, 'w', newline='')  as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(list_of_dict)
-
-
-def save_questions(filename, new_questions):
-    with open(filename, "w") as f:
-        f.write(','.join(note.question_dictionary_keys))
-        for question in new_questions:
-            f.write("\n" + ",".join(question.values()))
-
-
-def save_edited_question(updated_question_id, updated_title, updated_message, filename='question.csv'):
-    with open(filename, "r") as f:
-        reader = csv.DictReader(f)
-        new_questions = list(reader)
-        for question in new_questions:
-            if question["id"] == updated_question_id:
-                question["title"] = updated_title
-                question["message"] = updated_message
-    save_questions('question.csv', new_questions)
-
-
-def sort_questions(order_by, order_direction):
-    questions = open_file('question.csv')
-    if order_by == "id":
-        if order_direction == "asc":
-            return sorted(questions, key=itemgetter("id"))
-        elif order_direction == "desc":
-            return sorted(questions, key=itemgetter("id"), reverse=True)
-
-    elif order_by == "submission_time":
-        if order_direction == "asc":
-            return sorted(questions, key=itemgetter("submission_time"))
-        elif order_direction == "desc":
-            return sorted(questions, key=itemgetter("submission_time"), reverse=True)
-
-    elif order_by == "view_number":
-        if order_direction == "asc":
-            return sorted(questions, key=itemgetter("view_number"))
-        elif order_direction == "desc":
-            return sorted(questions, key=itemgetter("view_number"), reverse=True)
-
-    elif order_by == "vote_number":
-        if order_direction == "asc":
-            return sorted(questions, key=itemgetter("vote_number"))
-        elif order_direction == "desc":
-            return sorted(questions, key=itemgetter("vote_number"), reverse=True)
-
-    elif order_by == "title":
-        if order_direction == "asc":
-            return sorted(questions, key=itemgetter("title"))
-        elif order_direction == "desc":
-            return sorted(questions, key=itemgetter("title"), reverse=True)
-
-    elif order_by == "message":
-        if order_direction == "asc":
-            return sorted(questions, key=itemgetter("message"))
-        elif order_direction == "desc":
-            return sorted(questions, key=itemgetter("message"), reverse=True)
-
-
-def return_question_image_name(image_names, question_id):
-    question_image_name = "0"
-    for name in image_names:
-        if "question" + question_id in name and "answer" not in name:
-            question_image_name = name
-    return question_image_name
-
-
-def return_answer_image_names(image_names, question_id):
-    answer_image_names = []
-    for name in image_names:
-        if "question" + question_id in name and "answer" in name:
-            answer_image_names.append(name)
-    return answer_image_names
-
+@database_common.connection_handler
+def vote_down_question(cursor: RealDictCursor, id):
+    query="""
+    UPDATE question
+    SET vote_number = vote_number - 1
+    WHERE id=%(id)s
+    """
+    param = {'id': id}
+    cursor.execute(query, param)
+    return None
